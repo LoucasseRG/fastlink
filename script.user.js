929c44610c3faf668293fece4818567988d20bbc
Loucasse <takazyt@gmail.com>
2015-11-01 17:22:19 +0100

    Create script.user.js

---
+++ script.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Lien rapide RGT
+// @namespace    https://realitygaming.fr/
+// @include      https://realitygaming.fr/*
+// @version      0.1
+// @description  Lien rapide RGT
+// @author       Loucasse
+// @match        https://realitygaming.fr/*
+// @grant        none
+// ==/UserScript==.
+$(document).ready(function(){
+    $.getScript('https://rawgit.com/LoucasseRG/fastlink/master/script.js');
+});
